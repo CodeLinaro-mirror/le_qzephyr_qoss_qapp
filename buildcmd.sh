001fbe32bbeaf70554062643f710a0357b5e6a99
quic_ywenhua <quic_ywenhua@quicinc.com>
2025-08-21 18:12:16 +0800

    copy lib to prebuilt_HY11 folder
    Change-Id: Ia59e166f268e688c77d945bf827089584f8b9016

--- buildcmd.sh
+++ buildcmd.sh
@@ -7,11 +7,10 @@
 #========================================================================
 ws1="$(pwd)"
 echo $ws1
-cd ..
+cd ../
 if [ ! -f SRC-IOE-SDK.tar.gz ]; then
     tar --exclude=.git --exclude=.gitignore -czpf SRC-IOE-SDK.tar.gz modules/hal/cmsis modules/hal/qcom modules/lib/hostap \
-    zephyr prop/libcryptoqcc730 prop/libpowerqcc730 prop/libsensorqcc730 prop/libwifiqcc730 modules/fs/littlefs \
-    modules/crypto/mbedtls qapp qcc730 modules/debug/segger modules/lib/zcbor
+    zephyr modules/fs/littlefs modules/crypto/mbedtls qapp qcc730 modules/debug/segger modules/lib/zcbor
 fi
 cd zephyr
 newws="$(pwd)"
@@ -25,4 +24,15 @@ source ../build_qcc730mx.sh
 cd ../wifi_app
 source ../build_qcc730evbx.sh
 source ../build_qcc730mx.sh
-
+cd ../../
+ws2="$(pwd)"
+echo $ws2
+if [ ! -d prebuilt_HY11 ]; then
+    mkdir -p prebuilt_HY11
+    mkdir -p prebuilt_HY11_ART
+    cp ./modules/hal/qcom/zephyr/blobs/libsensorqcc730.a ./prebuilt_HY11/
+    cp ./modules/hal/qcom/zephyr/blobs/libcryptoqcc730.a ./prebuilt_HY11/
+    cp ./modules/hal/qcom/zephyr/blobs/libpowerqcc730.a ./prebuilt_HY11/
+    cp ./modules/hal/qcom/zephyr/blobs/libwifiqcc730.a ./prebuilt_HY11/
+    cp -r ./prebuilt_HY11/* ./prebuilt_HY11_ART/
+fi
