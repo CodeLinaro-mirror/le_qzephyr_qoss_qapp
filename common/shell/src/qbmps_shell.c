4262d6b6c26aa2190cdda42bef6bef106220a6e0
Long Cheng <longchen@qti.qualcomm.com>
2025-12-13 16:05:29 +0800

    power: Add bmps udp whitelist shell command
    CRs-Fixed:4384368
    Change-Id: Ifd69e930f674b4289f3acf37ad6f19af7f6f2062

--- common/shell/src/qbmps_shell.c
+++ common/shell/src/qbmps_shell.c
@@ -13,6 +13,7 @@
 #include <zephyr/device.h>
 #include "qurt_timer.h"
 #include <zephyr/kernel.h>
+#include "zephyr/net/net_ip.h"
 
 WMI_BMPS_ENABLE bmps;
 WMI_BMPS_IDLE_TIME idle_time;
@@ -22,6 +23,13 @@ static int cmd_clear_busy(const struct shell *ctx, size_t argc, char **argv);
 static int cmd_set_busy(const struct shell *ctx, size_t argc, char **argv);
 extern uint64_t bmps_duration;
 uint64_t bmps_start = 0;
+
+#define WIFI_MAC_HEADER_LEN 24
+#define LLC_SNAP_HEADER_LEN 8
+#define UDP_WHITELIST_LEN     4
+uint32_t udp_whitelist_arr[UDP_WHITELIST_LEN] = {7777, 0, 0, 0};
+typedef bool (*qapi_bmps_rx_filter_cb)(uint16_t type, bool bm_cast, void *pbuf, uint16_t len);
+extern bool (*wakeup_cb_dtim)(uint16_t type, bool bm_cast,void* pbuf,uint16_t len);
 
 K_TIMER_DEFINE(bmps_timer, bmps_timer_cb, NULL);
 static void bmps_timer_cb(struct k_timer *timer)
@@ -100,12 +108,16 @@ static int cmd_bmps_ignore_bcmc(const struct shell *ctx, size_t argc, char **arg
 {
     int err = 0;
     uint8_t enable = shell_strtoul(argv[1], 10, &err);
+    WMI_BMPS_IGNORE_BCMC *pdata = &bmps;
+
+    pdata->enable = enable;
 
     if (err) {
         shell_error(ctx, "Unable to parse enable (err %d)", err);
         return err;
     }
-    wmi_ignore_bcmc_in_bmps(NULL, enable);
+    /** wmi_ignore_bcmc_in_bmps(NULL, enable); */
+    wmi_cmd_send(WMI_BMPS_IGNORE_BCMC_CMDID, pdata, sizeof(*pdata));
     return 0;
 }
 
@@ -156,6 +168,143 @@ static int cmd_set_busy(const struct shell *ctx, size_t argc, char **argv)
     return 0;
 }
 
+bool wakeup_cb_bcmc_filter_dtim(uint16_t type, bool bm_cast, void *wifi_frame, uint16_t len)
+{
+    uint8_t *ip_frame;
+    if (bm_cast) {
+        if (len < (WIFI_MAC_HEADER_LEN + LLC_SNAP_HEADER_LEN)) {
+            return FALSE;
+
+        }
+
+        const uint8_t *llc_snap_header = wifi_frame + WIFI_MAC_HEADER_LEN;
+
+        if (llc_snap_header[6] != 0x08 || llc_snap_header[7] != 0x00) {
+            return TRUE;
+        }
+
+        ip_frame = (uint8_t *)wifi_frame + WIFI_MAC_HEADER_LEN + LLC_SNAP_HEADER_LEN;
+
+        struct net_ipv4_hdr  *ip = (struct net_ipv4_hdr  *)(ip_frame);
+        if (ip->proto != IPPROTO_UDP) {
+            return TRUE;
+        }
+
+        struct net_udp_hdr  *udp = (struct udp_hdr_hdr *)(ip_frame +NET_IPV4H_LEN);
+        uint16_t src_port = ntohs(udp->src_port);
+        uint16_t dst_port = ntohs(udp->dst_port);
+
+        // whitelist for UDP dst port
+        for (uint16_t i = 0; i < UDP_WHITELIST_LEN; i++) {
+            if (udp_whitelist_arr[i] && dst_port == udp_whitelist_arr[i]) {
+                return TRUE;
+            }
+        }
+
+        return FALSE;
+    }
+    return TRUE;
+}
+
+static  qapi_Status_t qapi_bmps_bcmc_rx_filter_cb_register(qapi_bmps_rx_filter_cb bmps_cb, qapi_bmps_rx_filter_cb net_cb)
+{
+    if(!bmps_cb)
+    {
+        return QAPI_ERR_INVALID_PARAM;
+    }
+
+    if(bmps_cb)
+    {
+        wakeup_cb_dtim = bmps_cb;
+    }
+
+    /** wakeup_cb_net = net_cb; */
+    
+
+   return QAPI_OK;
+}
+
+static int cmd_bcmc_enable(const struct shell *ctx, size_t argc, char **argv)
+{
+    int err = 0;
+    WMI_BMPS_ENABLE *pdata = (WMI_BMPS_ENABLE *)&bmps;
+    uint8_t enable = shell_strtoul(argv[1], 10, &err);
+
+    if (err) {
+        shell_error(ctx, "Unable to parse enable (err %d)", err);
+        return err;
+    }
+
+    memset(pdata, 0, sizeof(*pdata));
+    pdata->enable = enable;
+    wmi_cmd_send(WMI_BMPS_RX_FILTER_ENABLE_CMDID, pdata, sizeof(*pdata));
+
+    if (enable) {
+        qapi_bmps_bcmc_rx_filter_cb_register(wakeup_cb_bcmc_filter_dtim, NULL);
+
+    }
+
+
+
+    return 0;
+
+}
+
+static int cmd_set_bcmc_filter(const struct shell *ctx, size_t argc, char **argv)
+{
+	int opt;
+	int opt_index = 0;
+	struct getopt_state *state;
+	static const struct option long_options[] = {
+		{"adddel", required_argument, 0, 'a'},
+		{"udpport", required_argument, 0, 'u'},
+		{"querry", no_argument, 0, 'q'},
+		{0, 0, 0, 0}};
+	int en, port;
+
+
+	while ((opt = getopt_long(argc, argv, "a:u:q",
+				  long_options, &opt_index)) != -1) {
+		state = getopt_state_get();
+		switch (opt) {
+		case 'a':
+            en = atoi(state->optarg);
+            break;
+        case 'u':
+            port = atoi(state->optarg);
+            break;
+        case 'q':
+            shell_print(ctx, "udp whitelist port:\n");
+            for (int i = 0; i < UDP_WHITELIST_LEN; i++) {
+                shell_print(ctx, "[%d]: %d\n", i, udp_whitelist_arr[i]);
+            }
+            break;
+		case '?':
+		default:
+			shell_error(ctx, "Invalid option or option usage: %s\n",
+				 argv[opt_index + 1]);
+			return -ENOEXEC;
+        }
+    }
+
+        if (en) {
+            for (int i = 0; i < UDP_WHITELIST_LEN; i++) {
+                if (udp_whitelist_arr[i] == 0) {
+                    udp_whitelist_arr[i] = port;
+                    break;
+                }
+            }
+
+        } else {
+            for (int i = 0; i < UDP_WHITELIST_LEN; i++) {
+                if (udp_whitelist_arr[i] == port)
+                    udp_whitelist_arr[i] = 0;
+            }
+        }
+
+    return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_bmps_cmds,
                                SHELL_CMD_ARG(enable, NULL,
                                              "set bmps enable \n"
@@ -178,6 +327,17 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_bmps_cmds,
                                SHELL_CMD_ARG(set_busy, NULL,
                                              "set wifi busy\n",
                                              cmd_set_busy, 1, 0),
+                               SHELL_CMD_ARG(bcmc_enable, NULL,
+                                             "enable/disable bcmc filter\n"
+                                             "Usage: bcmc_enable 1/0, 1:enable, 0: disable\n",
+                                             cmd_bcmc_enable, 2, 0),
+                               SHELL_CMD_ARG(bcmc_list, NULL,
+                                             "Add/delete udp port to whitelist\n"
+                                             "Usage: bcmc_list [-a 1/0] [-u dst udp port] -q\n"
+                                             "[-a 1/0] 1: add udp port to the whitelist, 0: delte udp port from whitelist\n"
+                                             "[-u dst udp port]: the dst udp port like 7777\n"
+                                             "-q : query the whitelist\n",
+                                             cmd_set_bcmc_filter, 2, 3),
                                SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(qbmps, &sub_bmps_cmds, "bmps commands", NULL);
