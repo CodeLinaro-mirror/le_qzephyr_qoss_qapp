bc006a2e84afc257cf74ae8faf6c3931e21bc59a
lonchen <lonchen@qti.qualcomm.com>
2025-12-26 17:40:49 +0800

    [QCC730.ZE.1.0][FTM]FR115879: QDart support over uart interface for QCC730 zephyr ftm_app part  1. Move all source code and config to modules/hal/qcom/qftm
    Signed-off-by: lonchen <lonchen@qti.qualcomm.com>
    Change-Id: Iaf190cda6cfc0427bc49b308838f48d6380fb677

--- ftm_app/src/main.c
+++ ftm_app/src/main.c
@@ -12,36 +12,9 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <ftm_uart_polling.h>
 
-
-#if CONFIG_FTM_MODE
-#include "halphy_api.h"
-#endif
-
-
-#if CONFIG_FTM_MODE
-    app_mode_id_t app_mode = APP_MODE_FTM; 
-#else
-    app_mode_id_t app_mode = APP_MODE_MM; 
-#endif
-
-#if !IS_ENABLED(CONFIG_SHELL)
-    uint8_t min_loglvl = LOG_LEVEL_INF;
-#endif
-
-extern void SEGGER_RTT_Init (void);
-
-void main(void)
+int main(void)
 {
     printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
-    SEGGER_RTT_Init();
-
-#if CONFIG_FTM_MODE
-    printk("Build FTM image date and time: %s - %s\n", __DATE__, __TIME__);
-#endif
-
-    qwifi_init();
-
+    return 0;
 }
