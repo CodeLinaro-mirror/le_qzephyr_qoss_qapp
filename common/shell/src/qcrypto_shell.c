e7b8d4f5965272b0dcee561ee97c770359988782
xiaoxiang <xiaxia@qti.qualcomm.com>
2025-12-29 21:12:47 +0800

    add `qcrypto kdf_key`/`qcrypto kdf_encrypt`/`qcrypto kdf_decrypt` command
    Change-Id: I22d879b78230ab21a9627ced359cb903646ea5af

---
+++ common/shell/src/qcrypto_shell.c
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/pm/pm.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <CeML.h>
+#include <qapi_wlan_base.h>
+
+static int hex2digit(int c)
+{
+    if ('0' <= c && c <= '9') {
+        return c - '0';
+    } else if ('a' <= c && c <= 'f') {
+        return c - 'a' + 10;
+    } else if ('A' <= c && c <= 'F') {
+        return c - 'A' + 10;
+    }
+
+    return -EINVAL;
+}
+
+static int hexstr_to_digits(const char *data, uint8_t *buf, uint32_t n_buf)
+{
+    const uint32_t len = strlen(data);
+
+    if (len != 2 * n_buf) {
+        return -EINVAL;
+    }
+
+    for (int i = len - 1, k = 0; i >= 0; i--, k++) {
+        int t = hex2digit(data[i]);
+        if (t < 0) {
+            return -EINVAL;
+        }
+
+        if (k % 2 == 0) {
+            buf[k / 2] = t;
+        } else {
+            buf[k / 2] |= t << 4;
+        }
+    }
+
+    return 0;
+}
+
+static void hexdump(const struct shell *ctx, uint8_t *buf, uint32_t n_buf)
+{
+    for (int i = n_buf - 1; i >= 0; i--) {
+        shell_fprintf_normal(ctx, "%02x", buf[i]);
+    }
+    shell_fprintf_normal(ctx, "\n");
+}
+
+static int kdf_key_test_cmd(const struct shell *ctx, size_t argc, char **argv)
+{
+    uint8_t user_input[16] = {0};
+    uint64_t password = 0;
+    uint32_t result[4] = {0};
+    uint32_t result_len = sizeof(result) / sizeof(result[0]);
+
+    if (hexstr_to_digits(argv[1], user_input, 16)) {
+        shell_print(ctx, "Invalid kdf_key format, must be exactly 32(%d) hex chars\n", strlen(argv[1]));
+        return -EINVAL;
+    }
+
+    if (0 != CeML_hw_kdf(NULL, CEML_KDF_SECURE_STORAGE, user_input, 16, password, (uint32 *)result, result_len)) {
+        shell_print(ctx, "KDF Test Fail\n");
+        return -EINVAL;
+    }
+
+    shell_fprintf_normal(ctx, "KDF output: ");
+    for (int i = result_len - 1; i >= 0; i--) {
+        shell_fprintf_normal(ctx, "%08x", result[i]);
+    }
+    shell_fprintf_normal(ctx, "\n");
+
+    return 0;
+}
+
+static int kdf_encrypt_test_cmd(const struct shell *ctx, size_t argc, char **argv)
+{
+    uint8_t kdf_key[16] = {0};
+    uint8_t derive_key[16] = {0};
+    uint8_t iv[16] = {0};
+    uint32_t decrypt_len = 16;
+    uint32_t encrypt_len = 32;
+    uint8_t *encrypt_kdf = NULL;
+    uint8_t *encrypt_sw = NULL;
+    uint8_t *data_test = NULL;
+    qapi_Status_t ret = QAPI_ERROR;
+
+    if(hexstr_to_digits(argv[1], kdf_key, 16)) {
+        shell_print(ctx, "Invalid kdf_key, must be exactly 32(%d) hex chars", strlen(argv[1]));
+        return -EINVAL;
+    }
+
+    if(hexstr_to_digits(argv[2], derive_key, 16)) {
+        shell_print(ctx, "Invalid derive_key, must be exactly 32(%d) hex chars", strlen(argv[2]));
+        return -EINVAL;
+    }
+
+    data_test = (uint8_t*)malloc(decrypt_len + encrypt_len * 2);
+    if(!data_test) {
+        shell_print(ctx, "malloc for data of test fail\r\n");
+        goto error_back;
+    }
+
+    if(hexstr_to_digits(argv[3], data_test, decrypt_len)) {
+        shell_print(ctx, "Invalid data, must be exactly 32(%d) hex chars", strlen(argv[3]));
+        goto error_back;
+    }
+
+    encrypt_kdf = data_test + decrypt_len;
+    encrypt_sw = encrypt_kdf + encrypt_len;
+
+    memset(iv, 0x5a, sizeof(iv));
+    shell_fprintf_normal(ctx, "IV:\t\t");
+    hexdump(ctx, iv, sizeof(iv));
+
+    shell_fprintf_normal(ctx, "Input Data:\t");
+    hexdump(ctx, data_test, decrypt_len);
+
+	/* encrypt data with kdf key */
+    ret = CeML_util_encrypt_with_key(0, kdf_key, 16, iv, 16, data_test, decrypt_len, encrypt_kdf, (uint32 *)&encrypt_len);
+    if(ret) {
+        shell_print(ctx, "encrypt data with kdf key fail %d", ret);
+        goto error_back;
+    }
+    shell_fprintf_normal(ctx, "Encrypt Data(KDF Key):\t");
+    hexdump(ctx, encrypt_kdf, encrypt_len);
+
+    /* encrypt data with sw key */
+    ret = CeML_util_encrypt_with_key(1, derive_key, 16, iv, 16, data_test, decrypt_len, encrypt_sw, (uint32 *)&encrypt_len);
+    if(ret) {
+        shell_print(ctx, "encrypt data with sw key fail %d", ret);
+        goto error_back;
+    }
+    shell_fprintf_normal(ctx, "Encrypt Data(SW Key):\t");
+    hexdump(ctx, encrypt_sw, encrypt_len);
+
+    shell_fprintf_normal(ctx, "\"Encrypt Data(KDF Key)\" vs \"Encrypt Data(SW Key)\":\t");
+    if(memcmp(encrypt_kdf, encrypt_sw, encrypt_len)) {
+        shell_print(ctx, "not match");
+    }
+    else {
+        shell_print(ctx, "match");
+    }
+
+    ret = QAPI_OK;
+
+error_back:
+    if(data_test) {
+        free(data_test);
+    }
+
+    return ret;
+}
+
+static int kdf_decrypt_test_cmd(const struct shell *ctx, size_t argc, char **argv)
+{
+    uint8_t kdf_key[16] = {0};
+    uint8_t derive_key[16] = {0};
+    uint32_t encrypt_len = 32;
+    uint32_t decrypt_len = 16;
+    uint8_t *kdf_decrypted_data = NULL;
+    uint8_t *sw_decrypted_data = NULL;
+    uint8_t *encrypted_data = NULL;
+    qapi_Status_t ret = QAPI_ERROR;
+
+    if(hexstr_to_digits(argv[1], kdf_key, 16)) {
+        shell_print(ctx, "Invalid kdf_key, must be exactly 32(%d) hex chars", strlen(argv[1]));
+        return -EINVAL;
+    }
+
+    if(hexstr_to_digits(argv[2], derive_key, 16)) {
+        shell_print(ctx, "Invalid derive_key, must be exactly 32(%d) hex chars", strlen(argv[2]));
+        return -EINVAL;
+    }
+
+    encrypted_data = (uint8_t *) malloc(encrypt_len + decrypt_len * 2);
+    if(!encrypted_data) {
+        shell_print(ctx, "malloc for data of test fail\r\n");
+        goto error_back;
+    }
+    kdf_decrypted_data = encrypted_data + encrypt_len;
+    sw_decrypted_data = kdf_decrypted_data + decrypt_len;
+
+    if(hexstr_to_digits(argv[3], encrypted_data, encrypt_len)) {
+        shell_print(ctx, "Invalid encrypted_data, must be exactly 64(%d) hex chars", strlen(argv[3]));
+        goto error_back;
+    }
+
+    shell_fprintf_normal(ctx, "Encrypted Data:\t");
+    hexdump(ctx, encrypted_data, encrypt_len);
+
+    /* decrypt data with kdf key */
+    ret = CeML_util_decrypt_with_key(0, kdf_key, 16, encrypted_data, encrypt_len, kdf_decrypted_data, (uint32 *)&decrypt_len);
+    if(ret) {
+        shell_print(ctx, "decrypt data with kdf key fail %d", ret);
+        goto error_back;
+    }
+
+    shell_fprintf_normal(ctx, "Decrypt Data(KDF Key):\t");
+    hexdump(ctx, kdf_decrypted_data, decrypt_len);
+
+    /* decrypt data with sw key */
+    ret = CeML_util_decrypt_with_key(1, derive_key, 16, encrypted_data, encrypt_len, sw_decrypted_data, (uint32 *)&decrypt_len);
+    if(ret) {
+        shell_print(ctx, "decrypt data with sw key fail %d", ret);
+        goto error_back;
+    }
+
+    shell_fprintf_normal(ctx, "Decrypt Data(SW Key):\t");
+    hexdump(ctx, sw_decrypted_data, decrypt_len);
+
+    shell_fprintf_normal(ctx, "\"Decrypt Data(KDF Key)\" vs \"Decrypt Data(SW Key)\":\t");
+    if(memcmp(kdf_decrypted_data, sw_decrypted_data, decrypt_len)) {
+        shell_print(ctx, "not match");
+    }
+    else {
+        shell_print(ctx, "match");
+    }
+
+    ret = QAPI_OK;
+
+error_back:
+    if(encrypted_data) {
+        free(encrypted_data);
+    }
+
+    return ret;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_pm_cmds,
+                               SHELL_CMD_ARG(kdf_key, NULL,
+                                             "Test KDF derive key\n"
+                                             "Usage: kdf_key <kdf_key:str>\n"
+                                             "kdf_key: The length must be 32.\n"
+                                             "\tFor example: qcrypto kdftool \"27ef1314791dae3a7329f1433de0c303\"\n",
+                                             kdf_key_test_cmd, 2, 0),
+                               SHELL_CMD_ARG(kdf_encrypt, NULL,
+                                             "Test KDF Encrypt \n"
+                                             "Usage: kdf_encrypt <kdf_key:str> <derive_key:str> <data:str>\n"
+                                             "kdf_key: The length must be 32.\n"
+                                             "derive_key: The length must be 32.\n"
+                                             "data: The length must be 32.\n",
+                                             kdf_encrypt_test_cmd, 4, 0),
+                               SHELL_CMD_ARG(kdf_decrypt, NULL,
+                                             "Test KDF Decrypt \n"
+                                             "Usage: kdf_decrypt <kdf_key:str> <derive_key:str> <encrypted_data:str>\n"
+                                             "kdf_key: The length must be 32.\n"
+                                             "derive_key: The length must be 32.\n"
+                                             "encrypted_data: The length must be 64.\n",
+                                             kdf_decrypt_test_cmd, 4, 0),
+                               SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(qcrypto, &sub_pm_cmds, "crypto related commands", NULL);
