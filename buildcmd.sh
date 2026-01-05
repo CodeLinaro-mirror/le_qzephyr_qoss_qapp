dc2ce1d29a8492aba9cfd21fa65effdd101f7faf
Long Cheng <longchen@qti.qualcomm.com>
2026-01-05 14:20:26 +0800

    buildcmd: Add bmps and power app to ci build
    There are some different config between each app so add them to ci build
    should be necessary
    
    Change-Id: Ib666dc9daf96610e655be9ecf78d0bb8279feb93

--- buildcmd.sh
+++ buildcmd.sh
@@ -30,13 +30,26 @@ newws="$(pwd)"
 echo $newws
 git update-ref refs/heads/manifest-rev $(git rev-parse HEAD)
 cd ../
+
+basedirs="pwd"
+subdirs=(
+    "bmps_app"
+    "power_app"
+    "qcli_app"
+)
 west init -l qcc730
-cd qapp/qcli_app
-west build -b qcc730mi -d build/qcc730mi | tee build/build_qcc730mi.log
-west build -b qcc730mx -d build/qcc730mx | tee build/build_qcc730mx.log
-west build -b qcc730evbi -d build/qcc730evbi | tee build/build_qcc730evbi.log
-west build -b qcc730evbx -d build/qcc730evbx | tee build/build_qcc730evbx.log
-cd ../../
+cd qapp
+
+for d in "${subdirs[@]}"; do
+    echo "=== enter $d and build ==="
+    cd "$d"
+    west build -b qcc730mi -d build/qcc730mi | tee build/build_qcc730mi.log
+    west build -b qcc730mx -d build/qcc730mx | tee build/build_qcc730mx.log
+    west build -b qcc730evbi -d build/qcc730evbi | tee build/build_qcc730evbi.log
+    west build -b qcc730evbx -d build/qcc730evbx | tee build/build_qcc730evbx.log
+    cd ..
+done
+cd "$basedirs"
 ws2="$(pwd)"
 echo $ws2
 if [ ! -d prebuilt_HY11 ]; then
