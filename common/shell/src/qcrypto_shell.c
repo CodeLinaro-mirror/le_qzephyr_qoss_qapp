5ac5162c071cf0db610c650e07db5df413967710
Ni Zeng <nzeng@qti.qualcomm.com>
2026-01-30 14:54:51 +0800

    secure storage service support - qapp
    Description: support storing sensitive data in a way that is protected
    by hardware-backed security, not just by software encryption.
    
    Change-Id: Ica1a930ab4572e7f9d0c41c75fb958b28923898c
    Signed-off-by: Ni Zeng <nzeng@qti.qualcomm.com>

--- common/shell/src/qcrypto_shell.c
+++ common/shell/src/qcrypto_shell.c
@@ -20,6 +20,14 @@
 #include "mbedtls/sha256.h"
 #include "mbedtls/entropy.h"
 #include "entropy_poll.h"
+#if CONFIG_MBEDTLS_PSA_CRYPTO_C
+#include <psa/internal_trusted_storage.h>
+#endif
+
+#define USER_PASSWORD_SIZE 16
+#define QCRYPTO_TEST_DATA_FLAGS PSA_STORAGE_FLAG_NONE
+
+extern uint8_t g_securefs_password[16];
 
 static int hex2digit(int c)
 {
@@ -520,6 +528,270 @@ static int cmd_entropy_hw_poll(const struct shell *ctx, size_t argc, char **argv
 }
 #endif /* MBEDTLS_ENTROPY_HARDWARE_ALT */
 
+#if CONFIG_SECURE_STORAGE_ITS_IMPLEMENTATION_ZEPHYR
+static int cmd_secure_write(const struct shell *ctx, size_t argc, char **argv)
+{
+	int err = 0;
+    size_t data_length = 0;   /* length in bytes */
+    psa_storage_uid_t uid = 0;
+    uint8_t *data_write = NULL;   /* Data to be written to ITS. */
+    uint8_t *dup_hex = NULL;
+
+    if (argc != 3 && argc != 4) {
+        shell_error(ctx, "Usage: secure_write <uid> <hex_data> [optional: <length>]");
+        return -EINVAL;
+    }
+
+    uid = shell_strtoul(argv[1], 10, &err);
+    if (err) {
+        shell_error(ctx, "Invalid uid parameter (err %d)", err);
+        return err;
+    }
+
+    if (argc == 3) {
+        const char *hex_string = argv[2];
+        data_length = strlen(hex_string)/2;
+        if (data_length <= 0 || data_length > CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE) {
+            shell_error(ctx, "Invalid data length %zu bytes (max: %d bytes)", 
+                    data_length, CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE);
+            return -EINVAL;
+        }
+
+        data_write = (uint8_t *)malloc(data_length);
+        if (!data_write) {
+            shell_error(ctx, "Failed to allocate memory for data");
+            return -ENOMEM;
+        }
+
+        if(hexstr_to_digits(argv[2], data_write, data_length)) {
+            shell_error(ctx, "Invalid hex data, must be even-length");
+            return -EINVAL;
+        }
+    } else if (argc == 4) {
+        size_t hex_data_length = shell_strtoul(argv[3], 10, &err);
+        if (err) {
+            shell_error(ctx, "Invalid length parameter (err %d)", err);
+            return err;
+        }
+
+        if (hex_data_length % 2 != 0) {
+            shell_error(ctx, "The length of the hex string is %d, make sure to input even number of hex char.", hex_data_length);
+            return -EINVAL;
+        } else {
+            data_length = hex_data_length/2;
+        }
+
+        if (data_length <= 0 || data_length > CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE) {
+            shell_error(ctx, "Invalid data length %zu bytes (max: %d bytes)", 
+                    data_length, CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE);
+            return -EINVAL;
+        }
+
+        dup_hex = (uint8_t *)argv[2];
+        if (!dup_hex || !isxdigit((int)dup_hex[0]) || (strlen(dup_hex) != 1)) {
+            shell_error(ctx, "hex data must contain only [0-9] or [A-F]");
+            return -EINVAL;
+        }
+
+        data_write = (uint8_t *)malloc(data_length);
+        if (!data_write) {
+            shell_error(ctx, "Failed to allocate memory for data");
+            return -ENOMEM;
+        }
+
+        uint8_t nibble = (uint8_t)hex2digit(dup_hex[0]);
+        uint8_t byte_value = (nibble << 4) | nibble;   /* Single hex digit: "a" -> 0xaa */
+        for (size_t i = 0; i < data_length; i++) {
+            data_write[i] = byte_value;
+        }
+    }
+    
+	err = psa_its_set(uid, data_length, data_write, QCRYPTO_TEST_DATA_FLAGS);
+	if (err != PSA_SUCCESS) {
+		shell_error(ctx, "Writing the data to ITS failed. (%d)", err);
+        free(data_write);
+		return -1;
+	}
+
+    shell_print(ctx, "Successfully wrote %zu bytes to ITS", data_length);
+    free(data_write);
+
+	return 0;
+}
+
+static int cmd_secure_read(const struct shell *ctx, size_t argc, char **argv)
+{
+	int err = 0;
+    size_t data_length = 0;
+    size_t actual_length = 0;
+    psa_storage_uid_t uid = 0;
+    uint8_t *data_read = NULL;  /* Data to be read from ITS. */
+    size_t data_offset = 0;  /* Read back the data starting from an offset. */
+
+    if (argc != 3) {
+        shell_error(ctx, "Usage: secure_read <uid> <length>");
+        return -EINVAL;
+    }
+    
+    uid = shell_strtoul(argv[1], 10, &err);
+    if (err) {
+        shell_error(ctx, "Invalid uid parameter (err %d)", err);
+        return err;
+    }
+
+    size_t hex_data_length = shell_strtoul(argv[2], 10, &err);
+    if (err) {
+        shell_error(ctx, "Invalid length parameter (err %d)", err);
+        return err;
+    }
+
+    data_length = hex_data_length/2;
+    if (data_length <= 0 || data_length > CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE) {
+        shell_error(ctx, "Invalid data length %zu (max: %d)", 
+                    data_length, CONFIG_SECURE_STORAGE_ITS_MAX_DATA_SIZE);
+        return -EINVAL;
+    }
+
+    data_read = (uint8_t *)malloc(data_length);
+    if (!data_read) {
+        shell_error(ctx, "Failed to allocate memory for data");
+        return -ENOMEM;
+    }
+
+    err = psa_its_get(uid, data_offset, data_length, data_read, &actual_length);
+	
+	if (err != PSA_SUCCESS) {
+		shell_error(ctx, "Reading back the data from ITS failed. (%d).", err);
+        free(data_read);
+		return -1;
+	}
+
+    shell_print(ctx, "Successfully read %zu bytes from ITS:", actual_length);
+    shell_fprintf_normal(ctx, "Data: ");
+    for (int i = actual_length - 1; i >= 0; i--) {
+        shell_fprintf_normal(ctx, "%02x", data_read[i]);
+    }
+    shell_fprintf_normal(ctx, "\n");
+
+    free(data_read);
+
+	return 0;
+}
+
+static int cmd_secure_get_info(const struct shell *ctx, size_t argc, char **argv)
+{
+	int err = 0;
+    struct psa_storage_info_t info;
+    psa_storage_uid_t uid = 0;
+
+    if (argc != 2) {
+        shell_error(ctx, "Usage: secure_get_info <uid>");
+        return -EINVAL;
+    }
+
+    uid = shell_strtoul(argv[1], 10, &err);
+    if (err) {
+        shell_error(ctx, "Invalid uid parameter (err %d)", err);
+        return err;
+    }
+
+    err = psa_its_get_info(uid, &info);
+	if (err != PSA_SUCCESS) {
+		shell_error(ctx, "Failed to retrieve the entry's metadata. (%d)", err);
+		return -1;
+	}
+
+    shell_print(ctx, "size: %d", info.size);
+
+	return 0;
+}
+
+static int cmd_secure_remove(const struct shell *ctx, size_t argc, char **argv)
+{
+	int err = 0;
+    psa_storage_uid_t uid = 0;
+
+    if (argc != 2) {
+        shell_error(ctx, "Usage: secure_remove <uid>");
+        return -EINVAL;
+    }
+
+    uid = shell_strtoul(argv[1], 10, &err);
+    if (err) {
+        shell_error(ctx, "Invalid uid parameter (err %d)", err);
+        return err;
+    }
+
+    shell_print(ctx, "Removing the entry from ITS...");
+
+	err = psa_its_remove(uid);
+	if (err != PSA_SUCCESS) {
+		shell_error(ctx, "Failed to remove the entry. (%d)", err);
+		return -1;
+	}
+
+	shell_print(ctx,"Entry removed from ITS.");
+
+	return 0;
+}
+
+#define hex_to_dec_nibble(hex_nibble) ( \
+    ((hex_nibble >= '0' && hex_nibble <= '9') ? (hex_nibble - '0') : \
+    (hex_nibble >= 'a' && hex_nibble <= 'f') ? (hex_nibble - 'a' + 10) : \
+    (hex_nibble >= 'A' && hex_nibble <= 'F') ? (hex_nibble - 'A' + 10) : -1) )
+
+int convert_data_in_hex_to_byte_array(const char * data_in_hex, uint8_t * data_as_byte_array, uint32_t data_as_byte_array_size)
+{
+    uint32_t i;
+    const uint32_t data_in_hex_length = strlen(data_in_hex);
+
+    if (data_as_byte_array_size > (UINT32_MAX / 2)) {
+        return -1;
+    }
+
+    if ( (2*data_as_byte_array_size) != data_in_hex_length ) {
+        return -1;
+    }
+
+    for ( i = 0; i < data_as_byte_array_size; i++ ) {
+        int high_nibble = hex_to_dec_nibble(data_in_hex[2*i]);
+        int low_nibble = hex_to_dec_nibble(data_in_hex[2*i+1]);
+
+        if (high_nibble < 0 || low_nibble < 0) {
+            return -1;  // Invalid hex character
+        }
+
+        data_as_byte_array[i] = (high_nibble << 4) | low_nibble;
+    }
+
+    return 0;
+}
+
+static int cmd_secure_set_password(const struct shell *ctx, size_t argc, char **argv)
+{
+	int err = 0;
+    uint8_t user_password[USER_PASSWORD_SIZE];
+
+    if (argc != 2) {
+        shell_error(ctx, "Usage: secure_set_password <password_in_hex>");
+        return -EINVAL;
+    }
+
+    char *password_in_hex = argv[1];
+
+    int status_code = convert_data_in_hex_to_byte_array(password_in_hex, user_password, USER_PASSWORD_SIZE);
+    if (0 != status_code) {
+        shell_error(ctx, "Invalid password_in_hex, must be exactly 32 hex chars");
+        return -EINVAL;
+    } else {
+        memcpy(g_securefs_password, user_password, USER_PASSWORD_SIZE);
+        shell_info(ctx, "Set user password success, total 32 hex chars", password_in_hex);
+    }
+
+	return 0;
+}
+#endif /* CONFIG_SECURE_STORAGE_ITS_IMPLEMENTATION_ZEPHYR */
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_pm_cmds,
                                SHELL_CMD_ARG(kdf_key, NULL,
                                              "Test KDF derive key\n"
@@ -560,6 +832,28 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_pm_cmds,
                                              "Poll HW entropy bytes\n"
                                              "Usage: entropy_hw_poll <len>\n",
                                              cmd_entropy_hw_poll, 2, 0),
+#if CONFIG_SECURE_STORAGE_ITS_IMPLEMENTATION_ZEPHYR                                        
+                               SHELL_CMD_ARG(secure_write, NULL,
+                                             "encrypts the hex data and writes it with the provided uid\n"
+                                             "Usage: secure_write <uid> <hex_data> [optional: <length>]\n",
+                                             cmd_secure_write, 3, 1),
+                               SHELL_CMD_ARG(secure_read, NULL,
+                                             "reads and decrypts length bytes of data for the provided uid\n"
+                                             "Usage: secure_read <uid> <length>\n",
+                                             cmd_secure_read, 3, 0),
+                               SHELL_CMD_ARG(secure_get_info, NULL,
+                                             "get stored info\n"
+                                             "Usage: secure_get_info <uid>\n",
+                                             cmd_secure_get_info, 2, 0),
+                               SHELL_CMD_ARG(secure_remove, NULL,
+                                             "remove stored data for the provided uid\n"
+                                             "Usage: secure_remove <uid>\n",
+                                             cmd_secure_remove, 2, 0),
+                               SHELL_CMD_ARG(secure_set_password, NULL,
+                                             "set passowrd for securefs\n"
+                                             "Usage: secure_set_password <password_in_hex>\n",
+                                             cmd_secure_set_password, 2, 0),
+#endif /* CONFIG_SECURE_STORAGE_ITS_IMPLEMENTATION_ZEPHYR */
                                SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(qcrypto, &sub_pm_cmds, "crypto related commands", NULL);
